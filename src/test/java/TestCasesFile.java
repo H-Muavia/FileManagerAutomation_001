c3392e8c607d3ee098217b3675705da9ef84e3b3
Hasnain Muavia <Hasnain@funsol.cloud>
2023-09-26 10:13:44 +0500

    Initial commit

---
+++ src/test/java/TestCasesFile.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+
+public class TestCasesFile extends CreateDriverSession{
+
+
+//    public void main(){
+//        super.main();
+//    }
+
+
+    @Test
+    void ImagesTest(){
+
+        System.out.println("Images Folder Open");
+    }
+
+    @Test
+    void VideosTest(){
+        System.out.println("Video Folder Open");
+    }
+}
